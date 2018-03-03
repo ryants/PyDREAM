78dcd613c2890620e2f637b45d7f282465e3cf5f
Ryan Suderman <ryants@lanl.gov>
2018-03-03 10:17:03 -0700

    added timeout to likelihood calculation

--- pydream/model.py
+++ pydream/model.py
@@ -5,14 +5,26 @@ Created on Tue Jan 12 16:40:32 2016
 @author: Erin
 """
 
+from timeout_decorator import timeout, TimeoutError
+import numpy as np
+
+
 class Model():
-    
-    def __init__(self, likelihood, sampled_parameters):
+
+    likelihood_timeout = 360
+
+    def __init__(self, likelihood, sampled_parameters, l_timeout=None):
         self.likelihood = likelihood
+        if l_timeout:
+            self.likelihood_timeout = l_timeout
         if type(sampled_parameters) is list:
             self.sampled_parameters = sampled_parameters
         else:
             self.sampled_parameters = [sampled_parameters]
+
+    @timeout(seconds=likelihood_timeout)
+    def likelihood_with_timeout(self, q0):
+        return self.likelihood(q0)
         
     def total_logp(self, q0):
 
@@ -27,7 +39,10 @@ class Model():
                 prior_logp += param.prior(q0)
             var_start += param.dsize
 
-        loglike = self.likelihood(q0)
+        try:
+            loglike = self.likelihood(q0)
+        except TimeoutError:
+            loglike = -np.inf
          
         return prior_logp, loglike
     
